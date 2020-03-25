4808fa760949fe6287c3363625a0029069f5eaca
giorgio-p <info.giorgiopapitto@gmail.com>
2020-03-25 17:04:13 +0100

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -5,28 +5,33 @@ Created on Wed Mar 25 11:43:23 2020
 
 @author: papitto
 """
+
 from statsmodels.formula.api import ols
 import pandas as pd  
 
 input_file = '/home/raid2/papitto/Desktop/PsychoPy/MRep_2020_backup/MRep_training_backup/data/results.csv'
 dataset = pd.read_csv(input_file)
 
-dataset = dataset[["Subj_tr", "OT3_spec_Tot", "OT3_sub_Tot", "OT3_rule_Tot", "OT3_gen_Tot" ]]
+dataset = dataset[["Subj_tr", "OT3_spec_Tot", "OT3_sub_Tot", "OT3_rule_Tot", "OT3_gen_Tot", "RT3_spec_Tot", "RT3_sub_Tot", "RT3_rule_Tot", "RT3_gen_Tot" ]]
 
+####################
+#### ONSET TIME ####
+####  REACTION  ####
+####################
 
 OT_gen = dataset["OT3_gen_Tot"]
 OT_spec = dataset["OT3_spec_Tot"]
 OT_sub = dataset["OT3_sub_Tot"]
 OT_rule = dataset["OT3_rule_Tot"]
 
-dataset_spec = dataset[["Subj_tr", "OT3_spec_Tot"]]
-dataset_spec = dataset_spec.rename(columns={"OT3_spec_Tot": "OT"})
-dataset_sub = dataset[["Subj_tr", "OT3_sub_Tot"]]
-dataset_sub = dataset_sub.rename(columns={"OT3_sub_Tot": "OT"})
-dataset_rule = dataset[["Subj_tr", "OT3_rule_Tot"]]
-dataset_rule = dataset_rule.rename(columns={"OT3_rule_Tot": "OT"})
-dataset_gen = dataset[["Subj_tr", "OT3_gen_Tot"]]
-dataset_gen = dataset_gen.rename(columns={"OT3_gen_Tot": "OT"})
+dataset_spec = dataset[["Subj_tr", "OT3_spec_Tot", "RT3_spec_Tot"]]
+dataset_spec = dataset_spec.rename(columns={"OT3_spec_Tot": "OT","RT3_spec_Tot": "RT" })
+dataset_sub = dataset[["Subj_tr", "OT3_sub_Tot", "RT3_sub_Tot"]]
+dataset_sub = dataset_sub.rename(columns={"OT3_sub_Tot": "OT", "RT3_sub_Tot": "RT"})
+dataset_rule = dataset[["Subj_tr", "OT3_rule_Tot", "RT3_rule_Tot"]]
+dataset_rule = dataset_rule.rename(columns={"OT3_rule_Tot": "OT","RT3_rule_Tot": "RT"})
+dataset_gen = dataset[["Subj_tr", "OT3_gen_Tot", "RT3_gen_Tot"]]
+dataset_gen = dataset_gen.rename(columns={"OT3_gen_Tot": "OT","RT3_gen_Tot": "RT"})
 
 
 for i, row in dataset_spec.iterrows():
@@ -46,7 +51,14 @@ result_df['Sub']=result_df.condition.map({'sub':1,'spec':0,'rule':0, "gen": 0})
 result_df['Rule']=result_df.condition.map({'rule':1,'spec':0,'sub':0, "gen": 0})
 result_df['Gen']=result_df.condition.map({'gen':1,'spec':0,'rule':0, "sub": 0})
 
+###################
+####  FIT THE  ####
+####  MODEL   ####
+###################
 
-fit = ols('OT ~ C(Spec) + C(Sub) + C(Rule)+ C(Gen)', data=result_df).fit() 
+
+# I don´t know what is the difference here:
+#fit = ols('OT ~ C(Spec) + C(Sub) + C(Rule)+ C(Gen)', data=result_df).fit() 
+fit = ols("RT ~ C(condition)", data=result_df).fit() 
 
 fit.summary()
