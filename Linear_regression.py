3f6d563b68df1a35bea3561f4ddb343c77505b85
giorgio-p <info.giorgiopapitto@gmail.com>
2020-03-26 11:59:42 +0100

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -6,8 +6,10 @@ Created on Wed Mar 25 11:43:23 2020
 @author: papitto
 """
 
-from statsmodels.formula.api import ols
-import pandas as pd  
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import pandas as pd 
+import matplotlib.pyplot as plt
 
 input_file = '/home/raid2/papitto/Desktop/PsychoPy/MRep_2020_backup/MRep_training_backup/data/results.csv'
 dataset = pd.read_csv(input_file)
@@ -50,6 +52,7 @@ result_df['Spec']=result_df.condition.map({'spec':1,'sub':0,'rule':0, "gen": 0})
 result_df['Sub']=result_df.condition.map({'sub':1,'spec':0,'rule':0, "gen": 0})
 result_df['Rule']=result_df.condition.map({'rule':1,'spec':0,'sub':0, "gen": 0})
 result_df['Gen']=result_df.condition.map({'gen':1,'spec':0,'rule':0, "sub": 0})
+result_df['condition_num']=result_df.condition.map({'gen':4,'spec':1,'rule':3, "sub": 2})
 
 ###################
 ####  FIT THE  ####
@@ -58,7 +61,37 @@ result_df['Gen']=result_df.condition.map({'gen':1,'spec':0,'rule':0, "sub": 0})
 
 
 # I don´t know what is the difference here:
-#fit = ols('OT ~ C(Spec) + C(Sub) + C(Rule)+ C(Gen)', data=result_df).fit() 
-fit = ols("RT ~ C(condition)", data=result_df).fit() 
+y = result_df["OT"]
+x = result_df["condition"]
 
-fit.summary()
+model = smf.ols(formula="OT ~ C(condition)", data=result_df).fit()
+
+
+model.summary()
+
+fig, ax = plt.subplots()
+fig = sm.graphics.plot_fit(model, 0, ax=ax)
+
+
+
+
+###################
+####  PLOT THE  ###
+####  MEANS   ####
+###################
+
+
+OT_gen_mean = dataset["OT3_gen_Tot"].mean()
+OT_spec_mean = dataset["OT3_spec_Tot"].mean()
+OT_sub_mean = dataset["OT3_sub_Tot"].mean()
+OT_rule_mean = dataset["OT3_rule_Tot"].mean()
+
+    
+data = {'1_Spec': OT_spec_mean, '2_Sub': OT_sub_mean, '3_Rule': OT_rule_mean, '4_Gen': OT_gen_mean}
+names = ['1_Spec', '2_Sub', '3_Rule','4_Gen']
+values = [OT_spec_mean, OT_sub_mean, OT_rule_mean, OT_gen_mean]
+fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+axs[0].bar(names, values)
+axs[1].scatter(names, values)
+axs[2].plot(names, values)
+fig.suptitle('Categorical Plotting')
