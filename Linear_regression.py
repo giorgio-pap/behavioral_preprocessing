72f7a3c76bf6880092997b3942275815bce1d86a
giorgio-p <info.giorgiopapitto@gmail.com>
2020-03-27 12:42:03 +0100

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -21,11 +21,6 @@ dataset = dataset[["Subj_tr", "OT3_spec_Tot", "OT3_sub_Tot", "OT3_rule_Tot", "OT
 ####  REACTION  ####
 ####################
 
-OT_gen = dataset["OT3_gen_Tot"]
-OT_spec = dataset["OT3_spec_Tot"]
-OT_sub = dataset["OT3_sub_Tot"]
-OT_rule = dataset["OT3_rule_Tot"]
-
 dataset_spec = dataset[["Subj_tr", "OT3_spec_Tot", "RT3_spec_Tot"]]
 dataset_spec = dataset_spec.rename(columns={"OT3_spec_Tot": "OT","RT3_spec_Tot": "RT" })
 dataset_sub = dataset[["Subj_tr", "OT3_sub_Tot", "RT3_sub_Tot"]]
@@ -47,11 +42,16 @@ for i, row in dataset_gen.iterrows():
 
 frames = [dataset_spec, dataset_sub, dataset_rule, dataset_gen] 
 result_df = pd.concat(frames)    
+# so far it was identical to the previous script
 
+# now create dummy variables. Linear regression
+# methods usually work with numeric variables, so we convert 
+# categorical variables to numeric one
 result_df['Spec']=result_df.condition.map({'spec':1,'sub':0,'rule':0, "gen": 0})
 result_df['Sub']=result_df.condition.map({'sub':1,'spec':0,'rule':0, "gen": 0})
 result_df['Rule']=result_df.condition.map({'rule':1,'spec':0,'sub':0, "gen": 0})
 result_df['Gen']=result_df.condition.map({'gen':1,'spec':0,'rule':0, "sub": 0})
+
 result_df['condition_num']=result_df.condition.map({'gen':4,'spec':1,'rule':3, "sub": 2})
 
 ###################
@@ -59,20 +59,18 @@ result_df['condition_num']=result_df.condition.map({'gen':4,'spec':1,'rule':3, "
 ####  MODEL   ####
 ###################
 
-
-# I don´t know what is the difference here:
+# define the regression values to fit
 y = result_df["OT"]
 x = result_df["condition"]
 
+# this formula might be actually able to create dummy variables by its own
 model = smf.ols(formula="OT ~ C(condition)", data=result_df).fit()
 
-
+# really not sure if it is working though
 model.summary()
 
 fig, ax = plt.subplots()
 fig = sm.graphics.plot_fit(model, 0, ax=ax)
-
-
 
 
 ###################
@@ -80,13 +78,13 @@ fig = sm.graphics.plot_fit(model, 0, ax=ax)
 ####  MEANS   ####
 ###################
 
-
+# create the means for the Onset Times (OT)
 OT_gen_mean = dataset["OT3_gen_Tot"].mean()
 OT_spec_mean = dataset["OT3_spec_Tot"].mean()
 OT_sub_mean = dataset["OT3_sub_Tot"].mean()
 OT_rule_mean = dataset["OT3_rule_Tot"].mean()
 
-    
+# plot the data in three different plots   
 data = {'1_Spec': OT_spec_mean, '2_Sub': OT_sub_mean, '3_Rule': OT_rule_mean, '4_Gen': OT_gen_mean}
 names = ['1_Spec', '2_Sub', '3_Rule','4_Gen']
 values = [OT_spec_mean, OT_sub_mean, OT_rule_mean, OT_gen_mean]
